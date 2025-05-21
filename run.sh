a1864ff91aec524c02d5fb3b7ee35e94735d5e59
xb <ssbandjl@163.com>
2025-05-21 17:27:13 +0800

    update

--- run.sh
+++ run.sh
@@ -10,3 +10,19 @@ mpirun -np 2 -H s114:1,s116:1 \
     -x LD_LIBRARY_PATH \
     --allow-run-as-root \
     ./build/all_reduce_perf -b 8 -e 1G -f 2 -g 1
+
+
+# across 2 node, every node 2gpu:
+mpirun -np 4 -H s114:2,s116:2 \
+  -bind-to none -map-by slot \
+  -x NCCL_DEBUG=INFO \
+  -x NCCL_IB_DISABLE=0 \
+  -x LD_LIBRARY_PATH \
+  --allow-run-as-root \
+  ./build/all_reduce_perf -b 8 -e 1G -f 2 -g 1
+
+-np 4: total processes (GPUs)
+-H node1:2,node2:2: 2 GPUs on each node
+-g 1: 1 GPU per MPI rank
+-f 2: float32
+-b 8 -e 1G: range of message sizes
