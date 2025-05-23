51c5bba0eb1971f8f7c49d887377ccf729862c29
xb <ssbandjl@163.com>
2025-05-23 17:12:59 +0800

    update

--- run.sh
+++ run.sh
@@ -99,3 +99,29 @@ export LD_LIBRARY_PATH=/root/project/rdma/rdma-core/build/lib:/root/project/ai/n
 ./build/all_reduce_perf -b 8 -e 512M -f 2 -g 1
 
 
+
+mpirun -np 2 --hostfile hostfile ./run_nccl.sh
+
+
+
+mpirun -np 4 -H 192.168.1.10:2,192.168.1.11:2 \
+  -bind-to none -map-by slot \
+  --allow-run-as-root \
+  /root/project/ai/nccl-tests/run_with_rdma.sh
+
+# mpirun -np 4 --hostfile hostfile /root/project/ai/nccl-tests/run_with_rdma.sh
+
+mpirun -np 4 -H 192.168.1.10:2,192.168.1.11:2 \
+    -bind-to none -map-by slot \
+    --allow-run-as-root \
+    /root/project/ai/nccl-tests/run_with_rdma.sh
+
+
+mpirun -np 4 -H 192.168.1.10:2,192.168.1.11:2 \
+  -bind-to none -map-by slot \
+  -x NCCL_DEBUG=TRACE \
+  -x NCCL_IB_DISABLE=0 \
+  -x NCCL_NET=IB \
+  -x LD_LIBRARY_PATH=/root/project/rdma/rdma-core/build/lib:/root/project/ai/nccl-tests/nccl/build/lib \
+  --allow-run-as-root \
+  ./build/all_gather_perf -b 8 -e 1G -f 2 -g 1
