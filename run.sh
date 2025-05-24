b5180c85c26cc831ced1dddec817e77cdfff3cb3
xb <ssbandjl@163.com>
2025-05-24 08:47:09 +0800

    update

--- run.sh
+++ run.sh
@@ -145,3 +145,14 @@ mpirun -np 4 -H 192.168.1.10:2,192.168.1.11:2 \
   -x LD_LIBRARY_PATH=/root/project/rdma/rdma-core/build/lib:/root/project/ai/nccl-tests/nccl/build/lib \
   --allow-run-as-root \
   ./build/all_gather_perf -b 8 -e 1G -f 2 -g 1
+
+clear
+mpirun -np 4 -H 192.168.1.10:1,192.168.1.11:1,192.168.2.10:1,192.168.2.11:1 \
+  -bind-to none -map-by slot \
+  -x NCCL_DEBUG=INFO \
+  -x NCCL_IB_DISABLE=0 \
+  -x NCCL_NET=IB \
+  -x NCCL_DEBUG_SUBSYS=NET \
+  -x LD_LIBRARY_PATH=/root/project/rdma/rdma-core/build/lib:/root/project/ai/nccl-tests/nccl/build/lib \
+  --allow-run-as-root \
+  ./build/all_gather_perf -b 8 -e 1G -f 2 -g 1
