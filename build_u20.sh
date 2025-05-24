692e9d0caf001e2f7247fa303111cd5e84720856
xb <bin@sidynamic.com>
2025-05-24 20:18:10 +0800

    update

--- build_u20.sh
+++ build_u20.sh
@@ -17,7 +17,7 @@ export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 export MPI_HOME=/usr/lib/x86_64-linux-gnu/openmpi
 # ln -s /usr/bin/nvcc /usr/lib/cuda/bin/nvcc
 
-make MPI_HOME=$MPI_HOME CXXFLAGS="-I/usr/lib/x86_64-linux-gnu/openmpi/include -L/usr/lib/x86_64-linux-gnu/openmpi/lib -g -Og" MPI=1 CUDA_HOME=$CUDA_HOME NCCL_HOME=$NCCL_HOME
+make -j32 MPI_HOME=$MPI_HOME CXXFLAGS="-I/usr/lib/x86_64-linux-gnu/openmpi/include -L/usr/lib/x86_64-linux-gnu/openmpi/lib -g -Og" MPI=1 CUDA_HOME=$CUDA_HOME NCCL_HOME=$NCCL_HOME
 # make -j32 CXXFLAGS="-I/usr/lib/x86_64-linux-gnu/openmpi/include -L/usr/lib/x86_64-linux-gnu/openmpi/lib -g -Og" MPI=1 CUDA_HOME=/usr/lib/cuda DEBUG=1
 
 # ref: 
