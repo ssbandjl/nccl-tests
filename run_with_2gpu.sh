0b6972b27550e318f9b77031a2ac87bc3a2370e3
xb <ssbandjl@163.com>
2025-06-09 11:01:08 +0800

    update

--- run_with_2gpu.sh
+++ run_with_2gpu.sh
@@ -1,3 +1,5 @@
+# trace
+log_file="log/all_gather_perf_2gpu_$(date +'%Y_%m_%d_%H_%M_%S')_log"
 mpirun -np 2 -H 192.168.1.10:1,192.168.1.11:1 \
   -bind-to none -map-by slot \
   -x NCCL_DEBUG=TRACE \
@@ -7,4 +9,19 @@ mpirun -np 2 -H 192.168.1.10:1,192.168.1.11:1 \
   -x NCCL_NET_GDR_LEVEL=2 \
   -x LD_LIBRARY_PATH=/root/project/rdma/rdma-core/build/lib:/root/project/ai/nccl-tests/nccl/build/lib \
   --allow-run-as-root \
-  /root/project/ai/nccl-tests/build/all_gather_perf -b 8 -e 1G -f 2 -g 1 >> all_gather_perf_2gpu_log
+  /root/project/ai/nccl-tests/build/all_gather_perf -b 8 -e 1G -f 2 -g 1 > "$log_file" 2>&1
+
+
+# debug info
+log_file="log/all_gather_perf_2gpu_$(date +'%Y_%m_%d_%H_%M_%S')_log"
+mpirun -np 2 -H 192.168.1.10:1,192.168.1.11:1 \
+  -bind-to none -map-by slot \
+  -x NCCL_DEBUG=INFO \
+  -x NCCL_IB_DISABLE=0 \
+  -x NCCL_NET=IB \
+  -x NCCL_DEBUG_SUBSYS=NET \
+  -x NCCL_NET_GDR_LEVEL=2 \
+  -x LD_LIBRARY_PATH=/root/project/rdma/rdma-core/build/lib:/root/project/ai/nccl-tests/nccl/build/lib \
+  --allow-run-as-root \
+  /root/project/ai/nccl-tests/build/all_gather_perf -b 8 -e 1G -f 2 -g 1 > "$log_file" 2>&1
+
