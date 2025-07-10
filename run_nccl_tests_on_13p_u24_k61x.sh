280472140552a769c8f82af30be5708cd98a206a
xb <bin@chinachina.cn>
2025-07-10 16:53:54 +0800

    u

--- run_nccl_tests_on_13p_u24_k61x.sh
+++ run_nccl_tests_on_13p_u24_k61x.sh
@@ -25,26 +25,28 @@ COMMENT
 ALGO_LIST="all_reduce_perf"
 ALGO_LIST="all_gather_perf all_reduce_perf alltoall_perf broadcast_perf gather_perf hypercube_perf reduce_perf reduce_scatter_perf scatter_perf sendrecv_perf"
 for algo in ${ALGO_LIST};do
-  log_file=log/cx6dx/nccl_tests_${algo}_13p_u24_k61x_$(date +'%Y_%m_%d_%H_%M_%S')_log
+  log_file=log/13p/nccl_tests_${algo}_13p_u24_k61x_$(date +'%Y_%m_%d_%H_%M_%S')_log
 	echo $algo
 	echo $log_file
   mpirun -np 2 -H 192.168.1.10:1,192.168.1.11:1 \
-    -bind-to none -map-by slot \
-    -x NCCL_DEBUG=0 \
+    -bind-to core -map-by node \
+    -x NCCL_DEBUG=INFO \
     -x NCCL_IB_DISABLE=0 \
     -x NCCL_NET=IB \
     -x NCCL_IB_TIMEOUT=24 \
     -x NCCL_IB_HCA=xtrdma_0 \
     -x NCCL_IB_GID_INDEX=1 \
     -x NCCL_NET_GDR_LEVEL=SYS \
-    -x NCCL_IB_QPS_PER_CONNECTION=2 \
-    -x NCCL_DEBUG_SUBSYS=ALL \
+    -x NCCL_IB_QPS_PER_CONNECTION=4 \
+    -x NCCL_P2P_LEVEL=PIX \
+    -x NCCL_GDR_FLUSH_DISABLE=1 \
+    -x NCCL_MAX_NCHANNELS=1 \
     -x LD_LIBRARY_PATH=/root/project/rdma/dpu_user_rdma/build/lib:/root/project/ai/nccl-tests/nccl/build/lib \
     -x HUGE_PAGE_NUM=100 \
     -x XT_CQ_INLINE_CQE=0 \
     -x NCCL_TOPO_DUMP_FILE=topo_${algo}.xml \
     --allow-run-as-root \
-    /root/project/ai/nccl-tests/build/${algo} -b 8 -e 1G -w 30 -f 2 -g 1 > "$log_file" 2>&1
+    /root/project/ai/nccl-tests/build/${algo} -b 4 -e 1G -w 30 -f 2 -c 0 -g 1 -n 100 > "$log_file" 2>&1
 done
 
 # ALGO_LIST="all_gather_perf all_reduce_perf alltoall_perf broadcast_perf gather_perf hypercube_perf reduce_perf reduce_scatter_perf scatter_perf sendrecv_perf"
