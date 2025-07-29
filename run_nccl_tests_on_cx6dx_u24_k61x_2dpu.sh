148b1aaf0757e56d0f76744b03880b68b65122f6
xb <bin@chinachina.cn>
2025-07-29 14:41:57 +0800

    u

--- run_nccl_tests_on_cx6dx_u24_k61x_2dpu.sh
+++ run_nccl_tests_on_cx6dx_u24_k61x_2dpu.sh
@@ -15,68 +15,38 @@ unset DISPLAY
 export HWLOC_PLUGINS_BLACKLIST="gl,nvml"
 
 # Ref NCCL ENV
-export NCCL_ALGO=Ring | Tree
-export NCCL_PROTO=LL | Simple | LL128
+export NCCL_ALGO=Ring
+export NCCL_PROTO=LL
 
+-x NCCL_IB_AR_THRESHOLD=8388608 \
 CUDA_VISIBLE_DEVICES=1
-
-# make network better cross nodes
-NCCL_P2P_DISABLE=1
-NCCL_SHM_DISABLE=1
+-x NCCL_P2P_LEVEL=SYS # SYS（默认）	尽可能启用系统内所有 GPU 间的 P2P（如 NVLink、PCIe P2P）
 
 COMMENT
 
-ALGO_LIST="all_reduce_perf"
+ALGO_LIST="all_gather_perf"
 ALGO_LIST="all_gather_perf all_reduce_perf alltoall_perf broadcast_perf gather_perf hypercube_perf reduce_perf reduce_scatter_perf scatter_perf sendrecv_perf"
 ALGO_LIST="all_reduce_perf"
+mkdir -p log/cx6dx/2dpu/
 for algo in ${ALGO_LIST};do
-  log_file=log/13p/nccl_tests_${algo}_13p_u24_k61x_$(date +'%Y_%m_%d_%H_%M_%S')_log
+  log_file=log/cx6dx/2dpu/nccl_tests_${algo}_cx6dx_nic_pix_gpu_u24_k61x_2dpu_$(date +'%Y_%m_%d_%H_%M_%S')_log
 	echo $algo
 	echo $log_file
-  mpirun -np 8 -H 192.168.1.10:4,192.168.1.11:4\
-    -bind-to core -map-by node \
+  mpirun -np 4 -H 192.168.1.10:2,192.168.1.11:2,192.168.2.10:2,192.168.2.11:2 \
     -x NCCL_DEBUG=TRACE \
+    -x NCCL_DEBUG_SUBSYS=NET \
     -x NCCL_IB_DISABLE=0 \
     -x NCCL_NET=IB \
-    -x NCCL_IB_TIMEOUT=24 \
-    -x NCCL_IB_HCA=xtrdma_0 \
-    -x NCCL_IB_GID_INDEX=1 \
-    -x NCCL_P2P_DISABLE=1 \
-    -x NCCL_SHM_DISABLE=1 \
+    -x NCCL_IB_HCA=mlx5_0,mlx5_2 \
+    -x NCCL_IB_GID_INDEX=3 \
     -x NCCL_NET_GDR_LEVEL=SYS \
     -x NCCL_IB_QPS_PER_CONNECTION=4 \
-    -x NCCL_ALGO=Ring \
-    -x NCCL_PROTO=Simple \
-    -x NCCL_P2P_LEVEL=PIX \
     -x NCCL_GDR_FLUSH_DISABLE=1 \
     -x NCCL_MAX_NCHANNELS=1 \
-    -x LD_LIBRARY_PATH=/root/project/rdma/dpu_user_rdma/build/lib:/root/project/ai/nccl-tests/nccl/build/lib \
-    -x HUGE_PAGE_NUM=100 \
-    -x XT_CQ_INLINE_CQE=0 \
+    -x LD_LIBRARY_PATH=/root/project/ai/nccl-tests/nccl/build/lib \
     -x NCCL_TOPO_DUMP_FILE=topo_${algo}.xml \
     --allow-run-as-root \
-    /root/project/ai/nccl-tests/build/${algo} -b 4 -e 64 -w 30 -f 2 -c 0 -g 1 -n 100 > "$log_file" 2>&1
-  # mpirun -np 2 -H 192.168.1.10:1,192.168.1.11:1 \
-  #   -bind-to core -map-by node \
-  #   -x NCCL_DEBUG=INFO \
-  #   -x NCCL_IB_DISABLE=0 \
-  #   -x NCCL_NET=IB \
-  #   -x NCCL_IB_TIMEOUT=24 \
-  #   -x NCCL_IB_HCA=xtrdma_0 \
-  #   -x NCCL_IB_GID_INDEX=1 \
-  #   -x NCCL_NET_GDR_LEVEL=SYS \
-  #   -x NCCL_IB_QPS_PER_CONNECTION=4 \
-  #   -x NCCL_ALGO=Ring \
-  #   -x NCCL_PROTO=Simple \
-  #   -x NCCL_P2P_LEVEL=PIX \
-  #   -x NCCL_GDR_FLUSH_DISABLE=1 \
-  #   -x NCCL_MAX_NCHANNELS=1 \
-  #   -x LD_LIBRARY_PATH=/root/project/rdma/dpu_user_rdma/build/lib:/root/project/ai/nccl-tests/nccl/build/lib \
-  #   -x HUGE_PAGE_NUM=100 \
-  #   -x XT_CQ_INLINE_CQE=0 \
-  #   -x NCCL_TOPO_DUMP_FILE=topo_${algo}.xml \
-  #   --allow-run-as-root \
-  #   /root/project/ai/nccl-tests/build/${algo} -b 16M -e 16M -w 30 -f 2 -c 0 -g 1 -n 100 >> "$log_file" 2>&1
+    /root/project/ai/nccl-tests/build/${algo} -b 4 -e 1G -w 30 -f 2 -c 0 -g 1 -n 100 > "$log_file" 2>&1
 done
 
 # ALGO_LIST="all_gather_perf all_reduce_perf alltoall_perf broadcast_perf gather_perf hypercube_perf reduce_perf reduce_scatter_perf scatter_perf sendrecv_perf"
