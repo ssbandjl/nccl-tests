f22fffd4396b6c72f3d23a6efc004a1b3ace153f
xb <ssbandjl@163.com>
2025-07-29 11:35:01 +0800

    1

---
+++ scripts/ib_state.sh
@@ -0,0 +1,138 @@
+root@gdr116:~/project/ai/nccl-tests# ibstat
+CA 'xtrdma_0'
+        CA type: 
+        Number of ports: 1
+        Firmware version: 
+        Hardware version: 
+        Node GUID: 0x020102fffed50001
+        System image GUID: 0x020102fffed50001
+        Port 1:
+                State: Active
+                Physical state: LinkUp
+                Rate: 25
+                Base lid: 0
+                LMC: 0
+                SM lid: 0
+                Capability mask: 0x00050000
+                Port GUID: 0x020102fffed50001
+                Link layer: Ethernet
+CA 'xtrdma_1'
+        CA type: 
+        Number of ports: 1
+        Firmware version: 
+        Hardware version: 
+        Node GUID: 0x020102fffed50002
+        System image GUID: 0x020102fffed50002
+        Port 1:
+                State: Down
+                Physical state: Disabled
+                Rate: 25
+                Base lid: 0
+                LMC: 0
+                SM lid: 0
+                Capability mask: 0x00050000
+                Port GUID: 0x020102fffed50002
+                Link layer: Ethernet
+CA 'xtrdma_2'
+        CA type: 
+        Number of ports: 1
+        Firmware version: 
+        Hardware version: 
+        Node GUID: 0x020102fffed50003
+        System image GUID: 0x020102fffed50003
+        Port 1:
+                State: Active
+                Physical state: LinkUp
+                Rate: 25
+                Base lid: 0
+                LMC: 0
+                SM lid: 0
+                Capability mask: 0x00050000
+                Port GUID: 0x020102fffed50003
+                Link layer: Ethernet
+CA 'xtrdma_3'
+        CA type: 
+        Number of ports: 1
+        Firmware version: 
+        Hardware version: 
+        Node GUID: 0x020102fffed50004
+        System image GUID: 0x020102fffed50004
+        Port 1:
+                State: Down
+                Physical state: Disabled
+                Rate: 25
+                Base lid: 0
+                LMC: 0
+                SM lid: 0
+                Capability mask: 0x00050000
+                Port GUID: 0x020102fffed50004
+                Link layer: Ethernet
+CA 'xtrdma_4'
+        CA type: 
+        Number of ports: 1
+        Firmware version: 
+        Hardware version: 
+        Node GUID: 0x020102fffed50001
+        System image GUID: 0x020102fffed50001
+        Port 1:
+                State: Active
+                Physical state: LinkUp
+                Rate: 25
+                Base lid: 0
+                LMC: 0
+                SM lid: 0
+                Capability mask: 0x00050000
+                Port GUID: 0x020102fffed50001
+                Link layer: Ethernet
+CA 'xtrdma_5'
+        CA type: 
+        Number of ports: 1
+        Firmware version: 
+        Hardware version: 
+        Node GUID: 0x020102fffed50002
+        System image GUID: 0x020102fffed50002
+        Port 1:
+                State: Down
+                Physical state: Disabled
+                Rate: 25
+                Base lid: 0
+                LMC: 0
+                SM lid: 0
+                Capability mask: 0x00050000
+                Port GUID: 0x020102fffed50002
+                Link layer: Ethernet
+CA 'xtrdma_6'
+        CA type: 
+        Number of ports: 1
+        Firmware version: 
+        Hardware version: 
+        Node GUID: 0x020102fffed50003
+        System image GUID: 0x020102fffed50003
+        Port 1:
+                State: Active
+                Physical state: LinkUp
+                Rate: 25
+                Base lid: 0
+                LMC: 0
+                SM lid: 0
+                Capability mask: 0x00050000
+                Port GUID: 0x020102fffed50003
+                Link layer: Ethernet
+CA 'xtrdma_7'
+        CA type: 
+        Number of ports: 1
+        Firmware version: 
+        Hardware version: 
+        Node GUID: 0x020102fffed50004
+        System image GUID: 0x020102fffed50004
+        Port 1:
+                State: Down
+                Physical state: Disabled
+                Rate: 25
+                Base lid: 0
+                LMC: 0
+                SM lid: 0
+                Capability mask: 0x00050000
+                Port GUID: 0x020102fffed50004
+                Link layer: Ethernet
+root@gdr116:~/project/ai/nccl-tests# 
---
+++ scripts/nc_test.sh
@@ -0,0 +1,9 @@
+root@gdr116:~/project/ai/nccl-tests# nc -zv 192.168.1.11 22
+Connection to 192.168.1.11 22 port [tcp/ssh] succeeded!
+root@gdr116:~/project/ai/nccl-tests# nc -zv 192.168.1.10 22
+Connection to 192.168.1.10 22 port [tcp/ssh] succeeded!
+root@gdr116:~/project/ai/nccl-tests# nc -zv 192.168.2.10 22
+Connection to 192.168.2.10 22 port [tcp/ssh] succeeded!
+root@gdr116:~/project/ai/nccl-tests# nc -zv 192.168.2.11 22
+Connection to 192.168.2.11 22 port [tcp/ssh] succeeded!
+
