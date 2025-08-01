bf645e32678dd68130e4c1e5184b1436d51306d2
xb <bin@chinachina.cn>
2025-08-01 11:37:44 +0800

    u

--- run_nccl_tests_on_13p_u24_k61x_2dpu.sh
+++ run_nccl_tests_on_13p_u24_k61x_2dpu.sh
@@ -21,6 +21,8 @@ export NCCL_PROTO=LL | Simple | LL128
 CUDA_VISIBLE_DEVICES=1
 
 NCCL_IB_HCA=xtrdma_0:1,xtrdma_4:1, Use ports 1 of cards
+--oversubscribe
+--mca btl ^vader,ofi
 COMMENT
 
 ALGO_LIST="all_reduce_perf"
@@ -31,7 +33,7 @@ for algo in ${ALGO_LIST};do
   log_file=log/13p/2dpu/nccl_tests_${algo}_13p_u24_k61x_2dpu_$(date +'%Y_%m_%d_%H_%M_%S')_log
 	echo $algo
 	echo $log_file
-  mpirun -np 8 -H 192.168.1.10:2,192.168.1.11:2,192.168.2.10:2,192.168.2.11:2 -bind-to core --map-by slot --mca btl ^vader --report-bindings \
+  mpirun -np 8 -H 192.168.1.10:2,192.168.2.10:2,192.168.1.11:2,192.168.2.11:2 -bind-to core -map-by slot --oversubscribe --mca btl ^vader,ofi --report-bindings \
     -x NCCL_DEBUG=TRACE \
     -x NCCL_DEBUG_SUBSYS=NET \
     -x NCCL_IB_DISABLE=0 \
@@ -50,3 +52,32 @@ for algo in ${ALGO_LIST};do
     --allow-run-as-root \
     /root/project/ai/nccl-tests/build/${algo} -b 4 -e 64M -w 30 -f 2 -c 0 -g 1 -n 100 > "$log_file" 2>&1
 done
+
+
+# ALGO_LIST="all_reduce_perf"
+# ALGO_LIST="all_gather_perf all_reduce_perf alltoall_perf broadcast_perf gather_perf hypercube_perf reduce_perf reduce_scatter_perf scatter_perf sendrecv_perf"
+# ALGO_LIST="all_reduce_perf"
+# mkdir -p log/13p/2dpu/
+# for algo in ${ALGO_LIST};do
+#   log_file=log/13p/2dpu/nccl_tests_${algo}_13p_u24_k61x_2dpu_$(date +'%Y_%m_%d_%H_%M_%S')_log
+# 	echo $algo
+# 	echo $log_file
+#   mpirun -np 8 -H 192.168.1.10:2,192.168.2.10:2,192.168.1.11:2:192.168.2.11:2 -bind-to core --map-by slot --oversubscribe  --mca btl ^vader --report-bindings \
+#     -x NCCL_DEBUG=TRACE \
+#     -x NCCL_DEBUG_SUBSYS=NET \
+#     -x NCCL_IB_DISABLE=0 \
+#     -x NCCL_NET=IB \
+#     -x NCCL_IB_TIMEOUT=24 \
+#     -x NCCL_IB_HCA=xtrdma_0:1,xtrdma_4:1 \
+#     -x NCCL_IB_GID_INDEX=1 \
+#     -x NCCL_NET_GDR_LEVEL=SYS \
+#     -x NCCL_IB_QPS_PER_CONNECTION=4 \
+#     -x NCCL_GDR_FLUSH_DISABLE=1 \
+#     -x NCCL_MAX_NCHANNELS=4 \
+#     -x LD_LIBRARY_PATH=/root/project/rdma/dpu_user_rdma/build/lib:/root/project/ai/nccl-tests/nccl/build/lib \
+#     -x HUGE_PAGE_NUM=20 \
+#     -x XT_CQ_INLINE_CQE=0 \
+#     -x NCCL_TOPO_DUMP_FILE=topo_${algo}.xml \
+#     --allow-run-as-root \
+#     /root/project/ai/nccl-tests/build/${algo} -b 4 -e 64M -w 30 -f 2 -c 0 -g 1 -n 100 > "$log_file" 2>&1
+# done
